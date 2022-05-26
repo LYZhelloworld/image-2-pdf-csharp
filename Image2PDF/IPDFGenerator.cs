8f959b9a891af89de502b48a3ccd016155e440fa
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-05-27 00:10:21 +0800

    Add scaffold for PDF generator

---
+++ Image2PDF/IPDFGenerator.cs
@@ -0,0 +1,53 @@
+﻿namespace Image2PDF
+{
+    /// <summary>
+    /// The arguments of the event FileProcessedEvent.
+    /// </summary>
+    internal class FileProcessedEventArgs
+    {
+        public string Filename { get; set; } = default!;
+        public int Progress { get; set; }
+        public int Total { get; set; }
+    }
+
+    /// <summary>
+    /// The arguments of the event PDFGenerationCompletedEvent.
+    /// </summary>
+    internal class PDFGenerationCompletedEventArgs { }
+
+    /// <summary>
+    /// The interface of PDFGenerator.
+    /// </summary>
+    internal interface IPDFGenerator
+    {
+        /// <summary>
+        /// Generate PDF file.
+        /// </summary>
+        /// <param name="target">The PDF file location.</param>
+        public void Generate(string target);
+
+        /// <summary>
+        /// The handler of the event that a file has been processed.
+        /// </summary>
+        /// <param name="sender">The sender object, i.e., the Generator instance.</param>
+        /// <param name="e">The arguments.</param>
+        public delegate void FileProcessedEventHandler(object sender, FileProcessedEventArgs e);
+
+        /// <summary>
+        /// The event that a file has been processed.
+        /// </summary>
+        public event FileProcessedEventHandler? FileProcessedEvent;
+
+        /// <summary>
+        /// The handler of the event that the PDF has been generated.
+        /// </summary>
+        /// <param name="sender">The sender object, i.e., the Generator instance.</param>
+        /// <param name="e">The arguments.</param>
+        public delegate void PDFGenerationCompletedEventHandler(object sender, PDFGenerationCompletedEventArgs e);
+
+        /// <summary>
+        /// The event that the PDF has been generated.
+        /// </summary>
+        public event PDFGenerationCompletedEventHandler? PDFGenerationCompletedEvent;
+    }
+}
---
+++ Image2PDF/PDFGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Image2PDF
+{
+    internal class PDFGenerator : IPDFGenerator
+    {
+        public event IPDFGenerator.FileProcessedEventHandler? FileProcessedEvent;
+        public event IPDFGenerator.PDFGenerationCompletedEventHandler? PDFGenerationCompletedEvent;
+
+        public void Generate(string target)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Trigger the event when a file has been processed.
+        /// </summary>
+        /// <param name="filename">The filename that has been processed.</param>
+        /// <param name="progress">ID of the file processed.</param>
+        /// <param name="total">Total number of files.</param>
+        protected virtual void OnFileProcessedEvent(string filename, int progress, int total)
+        {
+            FileProcessedEvent?.Invoke(this, new FileProcessedEventArgs
+            {
+                Filename = filename,
+                Progress = progress,
+                Total = total,
+            });
+        }
+
+        protected virtual void OnPDFGenerationCompletedEvent()
+        {
+            PDFGenerationCompletedEvent?.Invoke(this, new PDFGenerationCompletedEventArgs());
+        }
+    }
+}
