33ded1ecdf0597e78ca86b1c99e0bb0d8527abfa
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-05-26 15:00:56 +0800

    Add scaffold code

--- Image2PDF/MainWindow.xaml.cs
+++ Image2PDF/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Image2PDF
 {
@@ -20,9 +9,62 @@ namespace Image2PDF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<string> filenames;
+
+        #region Commands
+        public static readonly RoutedCommand MoveUpCommand = new();
+        public static readonly RoutedCommand MoveDownCommand = new();
+        public static readonly RoutedCommand RemoveCommand = new();
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+
+            filenames = new List<string>();
+            filenameList.ItemsSource = filenames;
+
+            // test data
+            filenames.Add("File 1 File 1 File 1 File 1 File 1 File 1 File 1 File 1 File 1");
+            filenames.Add("File 2 File 1 File 1 File 1 File 1 File 1 File 1 File 1 File 1");
+            filenames.Add("File 3 File 1 File 1 File 1 File 1 File 1 File 1 File 1 File 1");
+
+        }
+
+        private void MoveUpCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var index = filenameList.SelectedIndex;
+            (filenames[index], filenames[index-1]) = (filenames[index-1], filenames[index]);
+            filenameList.Items.Refresh();
+        }
+
+        private void MoveUpCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = filenameList.SelectedIndex != -1 && filenameList.SelectedIndex > 0;
+        }
+
+        private void MoveDownCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var index = filenameList.SelectedIndex;
+            (filenames[index], filenames[index + 1]) = (filenames[index + 1], filenames[index]);
+            filenameList.Items.Refresh();
+        }
+
+        private void MoveDownCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = filenameList.SelectedIndex != -1 && filenameList.SelectedIndex < filenameList.Items.Count - 1;
+        }
+
+        private void RemoveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var index = filenameList.SelectedIndex;
+            filenames.RemoveAt(index);
+            filenameList.Items.Refresh();
+        }
+
+        private void RemoveCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = filenameList.SelectedIndex != -1;
         }
     }
 }
