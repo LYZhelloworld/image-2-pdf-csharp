94042a7e08ba46d31999304e3529ac5261700a26
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-05-27 14:09:37 +0800

    Set image size and put images on separate pages

--- Image2PDF/Generator/PDFGenerator.cs
+++ Image2PDF/Generator/PDFGenerator.cs
@@ -1,47 +1,21 @@
 ﻿using iText.IO.Image;
+using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
+using iText.Layout.Properties;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Image2PDF.PDFGenerator
 {
     internal class PDFGenerator : IPDFGenerator
     {
-        public event IPDFGenerator.FileProcessedEventHandler? FileProcessedEvent;
-        public event IPDFGenerator.PDFGenerationCompletedEventHandler? PDFGenerationCompletedEvent;
-
         private readonly IEnumerable<string> files;
 
-        /// <summary>
-        /// The constructor.
-        /// </summary>
-        /// <param name="files">The image filenames.</param>
-        public PDFGenerator(IEnumerable<string> files)
-        {
-            this.files = files;
-        }
-
-        public void Generate(string target)
-        {
-            PdfDocument pdfDoc = new(new PdfWriter(target));
-            Document doc = new(pdfDoc);
-
-            int index = 1;
-            foreach (string file in files)
-            {
-                Image img = new Image(ImageDataFactory.Create(file));
-                // add image to PDF
-                doc.Add(img);
-                // trigger event
-                OnFileProcessedEvent(file, index);
-                index++;
-            }
-
-            // completed
-            pdfDoc.Close();
-            OnPDFGenerationCompletedEvent(target);
-        }
+        #region Events
+        public event IPDFGenerator.FileProcessedEventHandler? FileProcessedEvent;
+        public event IPDFGenerator.PDFGenerationCompletedEventHandler? PDFGenerationCompletedEvent;
 
         /// <summary>
         /// Triggers the event when a file has been processed.
@@ -58,12 +32,65 @@ namespace Image2PDF.PDFGenerator
             });
         }
 
+        /// <summary>
+        /// Triggers the event when the PDF has been created.
+        /// </summary>
+        /// <param name="pdfFilename">The PDF filename.</param>
         protected virtual void OnPDFGenerationCompletedEvent(string pdfFilename)
         {
             PDFGenerationCompletedEvent?.Invoke(this, new PDFGenerationCompletedEventArgs
             {
                 PDFFilename = pdfFilename,
             });
+        }
+        #endregion
+
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="files">The image filenames.</param>
+        public PDFGenerator(IEnumerable<string> files)
+        {
+            this.files = files;
+        }
+
+        public void Generate(string target)
+        {
+            using (PdfDocument pdfDoc = new(new PdfWriter(target)))
+            {
+                using Document doc = new(pdfDoc);
+                // set margins to 0
+                doc.SetMargins(0, 0, 0, 0);
+                int index = 1;
+                foreach (string file in files)
+                {
+                    Image img = new Image(ImageDataFactory.Create(file));
+                    // add image to PDF
+                    getImageDimension(file, out int width, out int height);
+                    // 1px = 0.75pt
+                    pdfDoc.SetDefaultPageSize(new PageSize(width * .75f, height * .75f));
+                    if (index > 1) doc.Add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+                    doc.Add(img);
+                    // trigger event
+                    OnFileProcessedEvent(file, index);
+                    index++;
+                }
+            }
+            // completed
+            OnPDFGenerationCompletedEvent(target);
+        }
+
+        /// <summary>
+        /// Gets image dimension of an image file.
+        /// </summary>
+        /// <param name="filename">The filename of the image.</param>
+        /// <param name="width">The width of the image.</param>
+        /// <param name="height">The height of the image.</param>
+        private void getImageDimension(string filename, out int width, out int height)
+        {
+            using var fileStream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            using var img = System.Drawing.Image.FromStream(fileStream, false, false);
+            (width, height) = (img.Width, img.Height);
         }
     }
 }
