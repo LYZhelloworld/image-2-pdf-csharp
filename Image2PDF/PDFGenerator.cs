631d11763ec1cbdd0930b85235c203e7f1c831de
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-05-27 09:48:13 +0800

    Add PDF generation code

--- Image2PDF/PDFGenerator.cs
+++ Image2PDF/PDFGenerator.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
 
 namespace Image2PDF
 {
@@ -7,16 +10,44 @@ namespace Image2PDF
         public event IPDFGenerator.FileProcessedEventHandler? FileProcessedEvent;
         public event IPDFGenerator.PDFGenerationCompletedEventHandler? PDFGenerationCompletedEvent;
 
+        private readonly string[] files;
+
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="files">The image filenames.</param>
+        public PDFGenerator(string[] files)
+        {
+            this.files = files;
+        }
+
         public void Generate(string target)
         {
-            throw new NotImplementedException();
+            PdfDocument pdfDoc = new(new PdfReader(target));
+            Document doc = new(pdfDoc);
+
+            int index = 1;
+            int total = files.Length;
+            foreach (string file in files)
+            {
+                Image img = new Image(ImageDataFactory.Create(file));
+                // add image to PDF
+                doc.Add(img);
+                // trigger event
+                OnFileProcessedEvent(file, index, total);
+                index++;
+            }
+
+            // completed
+            pdfDoc.Close();
+            OnPDFGenerationCompletedEvent();
         }
 
         /// <summary>
         /// Trigger the event when a file has been processed.
         /// </summary>
         /// <param name="filename">The filename that has been processed.</param>
-        /// <param name="progress">ID of the file processed.</param>
+        /// <param name="progress">ID of the file processed, staring from 1.</param>
         /// <param name="total">Total number of files.</param>
         protected virtual void OnFileProcessedEvent(string filename, int progress, int total)
         {
