5be23f1b92e0b658e3bbca57815112fd6ab67f2e
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-07-07 22:28:54 +0800

    Extract model from main window

---
+++ Image2Pdf/Models/IMainWindowModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Image2Pdf.Generators;
+
+namespace Image2Pdf.Models;
+
+/// <summary>
+/// The model of <see cref="MainWindow"/>.
+/// </summary>
+public interface IMainWindowModel
+{
+    #region Properties
+    /// <summary>
+    /// The item source.
+    /// </summary>
+    IEnumerable<string> ItemSource { get; }
+    #endregion
+
+    #region Commands
+    /// <summary>
+    /// Adds image files to the list.
+    /// </summary>
+    /// <param name="files">The filenames to add.</param>
+    void AddFiles(params string[] files);
+
+    /// <summary>
+    /// Removes all files.
+    /// </summary>
+    void Clear();
+
+    /// <summary>
+    /// The command to move up an item.
+    /// </summary>
+    /// <param name="index">The index of the selected item.</param>
+    void MoveUp(int index);
+
+    /// <summary>
+    /// Indicates whether <see cref="MoveUp(int)"/> can be executed.
+    /// </summary>
+    /// <param name="index">The index of the selected item.</param>
+    bool CanMoveUp(int index);
+
+    /// <summary>
+    /// The command to move down an item.
+    /// </summary>
+    /// <param name="index">The index of the selected item.</param>
+    void MoveDown(int index);
+
+    /// <summary>
+    /// Indicates whether <see cref="MoveDown(int)"/> can be executed.
+    /// </summary>
+    /// <param name="index">The index of the selected item.</param>
+    bool CanMoveDown(int index);
+
+    /// <summary>
+    /// The command to remove an item.
+    /// </summary>
+    /// <param name="index">The index of the selected item.</param>
+    void Remove(int index);
+
+    /// <summary>
+    /// Indicates whether <see cref="Remove(int)"/> can be executed.
+    /// </summary>
+    /// <param name="index">The index of the selected item.</param>
+    bool CanRemove(int index);
+
+    /// <summary>
+    /// The command to remove an item.
+    /// </summary>
+    /// <param name="pdfFilename">The PDF filename.</param>
+    void Generate(string pdfFilename);
+
+    /// <summary>
+    /// Indicates whether <see cref="Generate"/> can be executed.
+    /// </summary>
+    bool CanGenerate();
+    #endregion
+
+    #region Events
+    /// <summary>
+    /// The event that a file has been processed.
+    /// </summary>
+    event EventHandler<FileProcessedEventArgs>? FileProcessedEvent;
+
+    /// <summary>
+    /// The event that the PDF has been generated.
+    /// </summary>
+    event EventHandler<PdfGenerationCompletedEventArgs>? PdfGenerationCompletedEvent;
+    #endregion
+}
---
+++ Image2Pdf/Models/MainWindowModel.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Image2Pdf.Generators;
+using Image2Pdf.Utilities;
+
+namespace Image2Pdf.Models;
+
+/// <summary>
+/// Implementation of <see cref="IMainWindowModel"/>.
+/// </summary>
+public class MainWindowModel : IMainWindowModel
+{
+    #region Fields
+    /// <summary>
+    /// The factory class of PDF generator.
+    /// </summary>
+    private readonly IPdfGeneratorFactory _pdfGeneratorFactory;
+
+    /// <summary>
+    /// The filenames of images.
+    /// </summary>
+    private readonly List<string> _filenames = new();
+    #endregion
+
+    #region Constructors
+    /// <summary>
+    /// The constructor.
+    /// </summary>
+    public MainWindowModel()
+        : this(new PdfGeneratorFactory())
+    {
+    }
+
+    /// <summary>
+    /// The constructor with all properties.
+    /// </summary>
+    /// <param name="pdfGeneratorFactory">The factory class of PDF generator.</param>
+    public MainWindowModel(IPdfGeneratorFactory pdfGeneratorFactory)
+    {
+        _pdfGeneratorFactory = pdfGeneratorFactory;
+    }
+    #endregion
+
+    #region Properties
+    /// <inheritdoc/>
+    public IEnumerable<string> ItemSource => _filenames;
+    #endregion
+
+    #region Methods
+    /// <inheritdoc/>
+    public void AddFiles(params string[] files)
+    {
+        // Add valid image files only. Ignore others.
+        _filenames.AddRange(files.Where(FileUtils.IsValidImageFile));
+    }
+
+    /// <summary>
+    /// Removes all files.
+    /// </summary>
+    public void Clear()
+    {
+        _filenames.Clear();
+    }
+    #endregion
+
+    #region Commands
+    /// <inheritdoc/>
+    public void MoveUp(int index)
+    {
+        (_filenames[index], _filenames[index - 1]) = (_filenames[index - 1], _filenames[index]);
+    }
+
+    /// <inheritdoc/>
+    public bool CanMoveUp(int index)
+    {
+        return index != -1 && index > 0;
+    }
+
+    /// <inheritdoc/>
+    public void MoveDown(int index)
+    {
+        (_filenames[index], _filenames[index + 1]) = (_filenames[index + 1], _filenames[index]);
+    }
+
+    /// <inheritdoc/>
+    public bool CanMoveDown(int index)
+    {
+        return index != -1 && index < _filenames.Count - 1;
+    }
+
+    /// <inheritdoc/>
+    public void Remove(int index)
+    {
+        _filenames.RemoveAt(index);
+    }
+
+    /// <inheritdoc/>
+    public bool CanRemove(int index)
+    {
+        return index != -1;
+    }
+
+    /// <inheritdoc/>
+    public void Generate(string pdfFilename)
+    {
+        // create PDF generator
+        IPdfGenerator<FileProcessedEventArgs, PdfGenerationCompletedEventArgs> pdfGenerator = _pdfGeneratorFactory.AddFiles(_filenames).Build();
+
+        pdfGenerator.FileProcessedEvent += (sender, e) => FileProcessedEvent?.Invoke(sender, e);
+        pdfGenerator.PdfGenerationCompletedEvent += (sender, e) => PdfGenerationCompletedEvent?.Invoke(sender, e);
+        Task.Run(() =>
+        {
+            pdfGenerator.Generate(pdfFilename);
+        });
+    }
+
+    /// <inheritdoc/>
+    public bool CanGenerate()
+    {
+        return _filenames.Count > 0;
+    }
+    #endregion
+
+    #region Events
+    /// <inheritdoc/>
+    public event EventHandler<FileProcessedEventArgs>? FileProcessedEvent;
+
+    /// <inheritdoc/>
+    public event EventHandler<PdfGenerationCompletedEventArgs>? PdfGenerationCompletedEvent;
+    #endregion
+}
